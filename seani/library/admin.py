ba94e3e8a680b8f4af054eadfa7d877737d71fd6
Daniel <danydge5@gmail.com>
2024-02-22 13:16:36 -0600

    04 implements Cloudinary customize Models

--- seani/library/admin.py
+++ seani/library/admin.py
@@ -2,7 +2,19 @@ from django.contrib import admin
 
 from .models import Module,Question
 
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'question_text', 'module', 'correct']
+    list_filter = ['module']
 
-# Register your models here.
-admin.site.register(Module)
-admin.site.register(Question)
+
+class QuestionInline(admin.StackedInline):
+    model = Question
+
+
+@admin.register(Module)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description', 'num_questions']
+    inlines = [QuestionInline]
+
+
--- seani/library/models.py
+++ seani/library/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from cloudinary.models import CloudinaryField
+
 # Create your models here.
 
 class Module(models.Model):
@@ -9,6 +11,12 @@ class Module(models.Model):
     description = models.CharField(
         verbose_name="Descripcion",
         max_length=200)
+    
+    
+    def num_questions(self):
+        return self.question_set.count()
+    num_questions.short_description = 'Numero de Preguntas'
+    
     
     def __str__(self):
         return self.name
@@ -25,10 +33,19 @@ class Question(models.Model):
     question_text = models.CharField(
         verbose_name = 'Texto de la pregunta',
         max_length=200, null=True, blank=True)
-    question_image = models.ImageField(
-        verbose_name='Imagen de la pregunta',
-        upload_to='questions', null=True, blank =True)
+    # question_image = models.ImageField(
+    #     verbose_name='Imagen de la pregunta',
+    #     upload_to='questions', null=True, blank =True)
     
+    question_image = CloudinaryField(
+        verbose_name = 'Imagen de la pregunta',
+        folder = 'questions',
+        resource_type = 'image',
+        null = True, blank = True
+        )
+
+
+
     answer1 = models.CharField(
         verbose_name='Respuesta A',
         max_length=200)
